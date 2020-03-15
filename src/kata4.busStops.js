f44a473af35a4936f6f1dfcdf5be39ee454958f1
aar.56aint <aaron.hays_7@yahoo.com>
2020-03-15 17:19:06 +0000

    kata4 refractored

--- src/kata4.busStops.js
+++ src/kata4.busStops.js
@@ -1,13 +1,21 @@
 //import { busStops } from '.';
+//import { busStops } from '.';
 const number = (busStops) => {
   //return busStops.map(i => i[0] - i[1]).reduce((a, b) => a + b);
-  return busStops.reduce((a, b) => a+b[0] - b[1], 0)
-    /*var totalPeople = 0;
-    for (var i=0; i<busStops.length; i++) {
+  //return busStops.reduce((a, b) => a+b[0] - b[1], 0)
+    /* let totalPeople = 0;
+    for (let i=0; i<busStops.length; i++) {
       totalPeople += busStops[i][0];
       totalPeople -= busStops[i][1];
     }
     return totalPeople;*/
+
+      let totalPeople = 0;
+     busStops.forEach(i => {
+       totalPeople += busStops[i][0];
+      totalPeople -= busStops[i][1];
+     })
+     return totalPeople 
   
 };
 
